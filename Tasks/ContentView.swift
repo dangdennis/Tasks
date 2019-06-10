c154f24ea22970fda5edb42cc4b26ca6d127d668
Dennis Dang <dangggdennis@gmail.com>
2019-06-09 23:14:11 -0700

    Initial Commit

---
+++ Tasks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Tasks
+//
+//  Created by Dennis Dang on 6/9/19.
+//  Copyright © 2019 Dennis Dang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
